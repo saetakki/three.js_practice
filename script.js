58a55dd51d6383d8070aaa1ce1e7f9e28f2cad36
세탁기 <116627038+saetakki@users.noreply.github.com>
2024-01-27 01:11:38 +0900

    lesson 03

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 import * as THREE from 'three';
 
+const canvas = document.querySelector('canvas.webgl');
+
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const mash = new THREE.Mesh(geometry, material);
+const mesh = new THREE.Mesh(geometry, material);
 
-scene.add(mash);
+scene.add(mesh);
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
